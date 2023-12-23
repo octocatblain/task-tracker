14efb1ce12fa081651c95248d5e91a1d53ee68e5
octocatblain <blainmasaku2@gmail.com>
2023-12-23 16:52:31 +0300

    feat: add useEffect and useState hooks

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CSS/Todo.css";
+
+const [todos, setTodos] = useState([]);
+const inputRef = useRef(null);
+
+let count = 0;
+const add = () => {
+  setTodos([
+    ...todos,
+    { no: count++, text: inputRef.current.value, display: "" },
+  ]);
+  inputRef.current.value = "";
+};
+useEffect(() => {
+  console.log(todos);
+}, [todos]);
 
 const Todo = () => {
   return (
     <div className="todo">
       <div className="todo-header">Task Tracker</div>
       <div className="todo-add">
-        <input type="text" className="todo-input" placeholder="Add a task" />
-        <div className="todo-button">Add</div>
+        <input
+          ref={inputRef}
+          type="text"
+          className="todo-input"
+          placeholder="Add a task"
+        />
+        <div
+          onClick={() => {
+            add();
+          }}
+          className="todo-button"
+        >
+          Add
+        </div>
       </div>
       <div className="todo-list"></div>
     </div>
